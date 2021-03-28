321df25c22a0c6115d7692825ef6c7164d764b54
Ahmad Almashni <ahmad.mashni@camp200.com>
2021-03-28 13:57:00 -0400

    remove urls

--- camp200/urls.py
+++ camp200/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('courses.urls'))
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
