8978807c41be29cd80bd6787601af3539584772d
oss.shoman <9991001920>
2021-03-29 00:03:29 +0300

    add views courses edit to courses html page create database to save result from html

--- classroom/models.py
+++ classroom/models.py
@@ -5,7 +5,7 @@ class Course(models.Model):
 	title = models.CharField(max_length=200)
 	description = models.TextField()
 	promo_video = models.CharField(max_length=200)
-	image_key = models.CharField(max_length=200)
+	image_key = models.CharField(max_length=200 ,blank=True)
 	price = models.FloatField()
 
 
