9f1a5c8f1714d37413a65aa69ffc90a832d485f5
Ahmad Almashni <ahmad.mashni@camp200.com>
2021-03-28 13:30:32 -0400

    Create models : Cousre, Section, Lesson Delete apps : Courses, Students Add postgresql configurations

--- camp200/settings.py
+++ camp200/settings.py
@@ -31,8 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'courses',
-    'students',
+    'classroom',
+    'dashboard',
+    'catalogue',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,13 +76,23 @@ WSGI_APPLICATION = 'camp200.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'camp200',
+        'USER': 'manager',
+        'PASSWORD': 'manager123',
+        'HOST': 'aurora.cjnwtxhdwz5m.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
--- courses/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from courses import views
-
-urlpatterns = [
-	path('', views.home)
-]
