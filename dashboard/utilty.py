57baf15c6985f1dd3f47ac9450a25ead00deaf72
Hamza <hamzaaabed3@gmail.com>
2021-03-31 02:39:33 +0300

    https://camp-200.atlassian.net/browse/CT1-36
    add vimeo duration to page
    edit the page
    edit the requirements.txt & settings.py

--- dashboard/utilty.py
+++ dashboard/utilty.py
@@ -1,15 +1,51 @@
-import boto3
 from django.conf import settings
+import boto3
+import vimeo
+import re
 
 
 class S3Manager:
-	AWS_ACCESS_KEY = settings.AWS_ACCESS_KEY
-	AWS_SECRET_KEY = settings.AWS_SECRET_KEY
+    AWS_ACCESS_KEY = settings.AWS_ACCESS_KEY
+    AWS_SECRET_KEY = settings.AWS_SECRET_KEY
 
-	def __init__(self):
-		self.client = boto3.client("s3",
-								   aws_access_key_id=self.AWS_ACCESS_KEY,
-								   aws_secret_access_key=self.AWS_SECRET_KEY)
+    def __init__(self):
+        self.client = boto3.client("s3",
+                                   aws_access_key_id=self.AWS_ACCESS_KEY,
+                                   aws_secret_access_key=self.AWS_SECRET_KEY)
 
-	def upload(self, file_obj, bucket, key):
-		self.client.upload_fileobj(file_obj, bucket, key)
+    def upload(self, file_obj, bucket, key):
+        self.client.upload_fileobj(file_obj, bucket, key)
+
+
+class VimeoManager:
+    client_identifier = settings.VIMEO_CLIENT_ID
+    token = settings.VIMEO_TOKEN
+    client_secrets = settings.VIMEO_CLIENT_SECRETS
+
+    def __init__(self):
+        self.v = vimeo.VimeoClient(
+            token=self.token,
+            key=self.client_identifier,
+            secret=self.client_secrets
+        )
+
+    def get_video_duration(self, video_id):
+        video_id =video_id
+        about_me = self.v.get(f'/videos/{video_id}')
+        duration = about_me.json()
+        return duration['duration']
+
+    def get_id_from_url(self, url):
+        """
+        example : https://vimeo.com/483817264
+            return: 483817264
+        """
+        result = re.search("(.com\/)([\d]+)", url)
+        video_id=result.group(2)
+        return video_id
+
+    def get_vimeo_duration(self, url):
+        video_id = self.get_id_from_url(url)
+        about_me = self.v.get(f'/videos/{video_id}')
+        duration = about_me.json()
+        return duration['duration']
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,81 +1,90 @@
 import re
+import time
+
 from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from classroom.models import Course, Lesson
-from dashboard.utilty import S3Manager
+from dashboard.utilty import S3Manager, VimeoManager
 
 
 def lesson(request, section_id, lesson_id=None):
-	context = {"section_id": section_id, "lesson_id": lesson_id}
-	if lesson_id:
-		context['lesson_obj'] = Lesson.objects.get(id=lesson_id)
+    context = {"section_id": section_id, "lesson_id": lesson_id}
+    if lesson_id:
+        lesson = Lesson.objects.get(id=lesson_id)
+        context['duration'] = time.gmtime(lesson.duration)
+        context['lesson_obj'] = lesson
 
-	return render(request, 'luma/Demos/Fixed_Layout/create_lesson.html', context)
+    return render(request, 'luma/Demos/Fixed_Layout/create_lesson.html', context)
 
 
 def create_or_update_lesson(request):
-	lesson_id = request.POST.get('lesson_id')
+    lesson_id = request.POST.get('lesson_id')
+    defaults = {'title': request.POST["title"],
+                'type': request.POST["type"],
+                'link': request.POST['link'],
+                'section_id': request.POST['section_id']}
 
-	defaults = {'title': request.POST["title"],
-				'type': request.POST["type"],
-				'link': request.POST['link'],
-				'section_id': request.POST['section_id']}
+    if request.POST['type'] == 'Uploaded Video':
+        defaults['duration'] = VimeoManager().get_vimeo_duration(request.POST['link'])
 
-	Lesson.objects.update_or_create(id=lesson_id, defaults=defaults)
+    Lesson.objects.update_or_create(id=lesson_id, defaults=defaults)
 
-	messages.add_message(request, messages.INFO, 'Hello world.')
-	return redirect('lesson', section_id=request.POST['section_id'])
+    messages.add_message(request, messages.INFO, '')
+    return redirect('lesson', section_id=request.POST['section_id'])
 
 
 def courses(request):
-	courses = Course.objects.all()
-	context = {"courses": courses}
-	return render(request, 'luma/Demos/Fixed_Layout/instructor-courses.html', context)
+    courses = Course.objects.all()
+    context = {"courses": courses}
+    return render(request, 'luma/Demos/Fixed_Layout/instructor-courses.html', context)
 
 
 def course_crud(request, course_id=None):
-	if request.method == 'POST':
-		course_id = request.POST.get('course_id')
+    if request.method == 'POST':
+        course_id = request.POST.get('course_id')
 
-		Title = request.POST['Course_title']
-		Description = request.POST['Description']
-		Video_link = request.POST['Video_link']
-		Price = request.POST['price']
-		fileToUpload = request.FILES.get('image_file')
-		if course_id:
-			image_key = str(course_id) + '.jpg'
+        Title = request.POST['Course_title']
+        Description = request.POST['Description']
+        Video_link = request.POST['Video_link']
+        Price = request.POST['price']
+        fileToUpload = request.FILES.get('image_file')
+        if course_id:
+            image_key = str(course_id) + '.jpg'
 
-			course = Course.objects.filter(id=course_id).update(title=Title, description=Description,
-																promo_video=Video_link, price=Price)
+            course = Course.objects.filter(id=course_id).update(title=Title, description=Description,
+                                                                promo_video=Video_link, price=Price)
 
-			if request.FILES.get('image_file'):
-				s3 = S3Manager()
-				s3.upload(fileToUpload, settings.COURSES_IMAGES_BUCKET, image_key)
+            if request.FILES.get('image_file'):
+                s3 = S3Manager()
+                s3.upload(fileToUpload, settings.COURSES_IMAGES_BUCKET, image_key)
 
-			return redirect(course_crud, course_id=course_id)
+            return redirect(course_crud, course_id=course_id)
 
-		else:
-			s3 = S3Manager()
-			course = Course.objects.create(title=Title, description=Description, promo_video=Video_link, price=Price)
+        else:
+            s3 = S3Manager()
+            course = Course.objects.create(title=Title, description=Description, promo_video=Video_link, price=Price)
 
-			image_key = str(course.id) + '.jpg'
-			s3.upload(fileToUpload, settings.COURSES_IMAGES_BUCKET, image_key)
+            image_key = str(course.id) + '.jpg'
+            s3.upload(fileToUpload, settings.COURSES_IMAGES_BUCKET, image_key)
 
-			Course.objects.filter(id=course.id).update(image_key=image_key)
-			return redirect(course_crud, course_id=course.id)
-	elif request.method == 'GET':
-		context = {}
-		if course_id:
-			context['course'] = Course.objects.get(id=course_id)
-		return render(request, 'luma/Demos/Fixed_Layout/instructor-edit-course.html', context=context)
+            Course.objects.filter(id=course.id).update(image_key=image_key)
+            return redirect(course_crud, course_id=course.id)
+    elif request.method == 'GET':
+        context = {}
+        if course_id:
+            context['course'] = Course.objects.get(id=course_id)
+        return render(request, 'luma/Demos/Fixed_Layout/instructor-edit-course.html', context=context)
 
 
 def course_view(request, course_id=None):
-	# Show course View
-	# Get course from database
-	course = Course.objects.get(id=course_id)
-	vimeo_video_id = re.search('(\/)(\d+$)', course.promo_video)
-	context = {"course": course, 'vimeo_id': vimeo_video_id.group(2)}
-	print(context)
-	return render(request, 'luma/Demos/Fixed_Layout/dashboard-show-course.html', context)
+    # Show course View
+    # Get course from database
+    course = Course.objects.get(id=course_id)
+    vimeo_video_id = re.search('(\/)(\d+$)', course.promo_video)
+    context = {"course": course, 'vimeo_id': vimeo_video_id.group(2)}
+    print(context)
+    return render(request, 'luma/Demos/Fixed_Layout/dashboard-show-course.html', context)
+
+
+
