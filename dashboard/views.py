dd64671a90dad42131de2e4c2d2a45612e4ecf35
Omar Dandan <omardandan74@gmail.com>
2021-03-31 17:51:44 +0300

    add course time add course duraion add lesson count

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,6 +4,7 @@ import time
 from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib import messages
+
 from classroom.models import Course, Lesson, Section
 from dashboard.utilty import S3Manager, VimeoManager
 from classroom.models import Course, Lesson
@@ -64,42 +65,59 @@ def course_crud(request, course_id=None):
 
     return redirect(course_form, course_id=course.id)
 
-def course_form(request,course_id=None):
-	context ={}
-	if course_id:
-		course = Course.objects.get(id=course_id)
-		re_result = re.search('(.com\/)([\d]+)', course.promo_video)
-		print(re_result)
-		context = {'course': Course.objects.get(id=course_id), 'vimeo_id': re_result.group(2), 'image_key': course.image_key,'BUCKET':settings.COURSES_IMAGES_BUCKET}
-	return render(request, 'luma/Demos/Fixed_Layout/instructor-edit-course.html', context=context)
+
+def course_form(request, course_id=None):
+    context = {}
+    if course_id:
+        course = Course.objects.get(id=course_id)
+        re_result = re.search('(.com\/)([\d]+)', course.promo_video)
+        print(re_result)
+        context = {'course': Course.objects.get(id=course_id), 'vimeo_id': re_result.group(2),
+                   'image_key': course.image_key, 'BUCKET': settings.COURSES_IMAGES_BUCKET}
+    return render(request, 'luma/Demos/Fixed_Layout/instructor-edit-course.html', context=context)
 
 
 def course_view(request, course_id=None):
-
     # Show course View
     # Get course from database
     course = Course.objects.get(id=course_id)
     vimeo_video_id = re.search('(\/)(\d+$)', course.promo_video)
-    context = {"course": course, 'vimeo_id': vimeo_video_id.group(2)}
+    lessons = Lesson.objects.filter(section__course_id=course_id)
+    lessons_count = Lesson.objects.filter(section__course_id=course_id).count()
+
+    course_duraion = 0
+    for l in lessons:
+        course_duraion += l.duration
+    course_time = time.gmtime(course_duraion)
+
+    context = {"course": course,
+               'vimeo_id': vimeo_video_id.group(2),
+               "lessons_count": lessons_count,
+               "course_duraion": course_duraion,
+               "course_time": course_time
+               }
     return render(request, 'luma/Demos/Fixed_Layout/dashboard-show-course.html', context)
 
-def create_section(request,course_id=None,section_id=None):
-	context={}
-	if course_id:
-		context = {'course_id': course_id,'section_id':section_id}
-	if section_id:
-		context['section'] = Section.objects.get(id=section_id)
-	return render(request ,"luma\Demos\Fixed_Layout\instructor-add-section.html" ,context =context)
+
+def create_section(request, course_id=None, section_id=None):
+    context = {}
+    if course_id:
+        context = {'course_id': course_id, 'section_id': section_id}
+    if section_id:
+        context['section'] = Section.objects.get(id=section_id)
+    return render(request, "luma\Demos\Fixed_Layout\instructor-add-section.html", context=context)
+
 
 def create_or_update_section(request):
-	section_id = request.POST.get('section_id')
-	section_title = request.POST['section_title']
-	course_id = request.POST['course_id']
+    section_id = request.POST.get('section_id')
+    section_title = request.POST['section_title']
+    course_id = request.POST['course_id']
 
-	defaults = {'title': section_title, 'course_id': course_id}
-	section, created = Section.objects.update_or_create(id =section_id , defaults = defaults)
+    defaults = {'title': section_title, 'course_id': course_id}
+    section, created = Section.objects.update_or_create(id=section_id, defaults=defaults)
 
-	return redirect(create_section,course_id=course_id, section_id= section.id)
+    return redirect(create_section, course_id=course_id, section_id=section.id)
+
 
 def delete_course(request, course_id):
     image_key = S3ObjectsFormatters.course_image(course_id)
